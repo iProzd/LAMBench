d47f00a2d4f317a86b25951e343c03c8e8e933ac
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2024-12-13 21:18:58 +0800

    chore: remove ase task

--- lambench/tasks/__init__.py
+++ lambench/tasks/__init__.py
@@ -1,8 +1,6 @@
 from lambench.tasks.direct.direct_predict import DirectPredictTask
 from lambench.tasks.finetune.property_finetune import PropertyFinetuneTask
-from lambench.tasks.direct.direct_predict_ase import DirectPredictASETask
 __all__ = [
     "DirectPredictTask",
-    "DirectPredictASETask",
     "PropertyFinetuneTask",
 ]
--- lambench/tasks/direct/direct_predict_ase.py
+++
@@ -1,7 +0,0 @@
-from lambench.tasks.direct.direct_predict import DirectPredictTask
-import logging
-
-class DirectPredictASETask(DirectPredictTask):      
-    def run_task(self, model):
-        pass
-        
