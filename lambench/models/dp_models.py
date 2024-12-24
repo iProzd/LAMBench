f3d3344a9e9eb51ffe91eb0a9662656482f60ca1
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2024-12-24 15:30:50 +0800

    chore: rename var

--- lambench/models/dp_models.py
+++ lambench/models/dp_models.py
@@ -7,7 +7,7 @@ from lambench.tasks.utils import read_dptest_log_file
 class DPModel(BaseLargeAtomModel):
 
     #this need to be modified based on tasks
-    DP_ID_OOD: Dict = {
+    DP_TASK_CONFIG: Dict = {
         "Example_task_A" :("head_A", True), # head, change_bias
     }
 
@@ -19,10 +19,10 @@ class DPModel(BaseLargeAtomModel):
     def evaluate(self, task_name:str, test_file_path: str, target_name: str) -> dict:
         if target_name != "standard":
             self._finetune()
-        if task_name not in self.DP_ID_OOD:
+        if task_name not in self.DP_TASK_CONFIG:
             logging.error(f"Task {task_name} is not specified by DPModel")
             return {}
-        head, change_bias = self.DP_ID_OOD[task_name]
+        head, change_bias = self.DP_TASK_CONFIG[task_name]
         if change_bias:
             self._change_bias(test_file_path, head)
         else:
