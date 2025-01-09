b7d6b307f52e147d271d67e8ef5eaf034001573e
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2025-01-09 13:27:31 +0800

    fix: deregister force from WBM dpdata

--- dev/ase_test_interface.py
+++ dev/ase_test_interface.py
@@ -2,13 +2,33 @@ from pymatgen.core import Structure
 from pymatgen.io.ase import AseAtomsAdaptor
 import numpy as np
 import dpdata
+from dpdata.data_type import (
+    Axis,
+    DataType,
+)
 import glob
 from pathlib import Path
 from lamstare.infra.ood_database import OODRecord
 
+
+def deregister_data_type():
+    """
+    Deregister data type to avoid conflict.
+    This should only be used in the WBM dataset which does not have force labels.
+    """
+    datatype = DataType(
+        "forces",
+        np.ndarray,
+        shape=(Axis.NFRAMES, Axis.NATOMS, 1),
+        required=False,
+    )
+    dpdata.System.register_data_type(datatype)
+    dpdata.LabeledSystem.register_data_type(datatype)
+
+
 def run_ase_dptest(
-        calc,
-        testpath: str,
+    calc,
+    testpath: str,
 ):
     """
     Given a ASE calculator and a test path, run ASE dptest and return the results.
@@ -29,18 +49,19 @@ def run_ase_dptest(
     for path in testpath:
         systems.extend(glob.glob(f"{path}/*"))
     # check if the system is mixed type
-    if len(glob.glob(systems[0] + '/**/real_atom_types.npy', recursive=True)) == 0:
+    if len(glob.glob(systems[0] + "/**/real_atom_types.npy", recursive=True)) == 0:
         mix_type = False
     else:
         mix_type = True
 
-
     for filepth in systems:
+        if "WBM" in filepth:
+            deregister_data_type()
         if mix_type:
             sys = dpdata.MultiSystems()
-            sys.load_systems_from_file(filepth, fmt='deepmd/npy/mixed')
+            sys.load_systems_from_file(filepth, fmt="deepmd/npy/mixed")
         else:
-            sys = dpdata.LabeledSystem(filepth, fmt='deepmd/npy')
+            sys = dpdata.LabeledSystem(filepth, fmt="deepmd/npy")
 
         for ls in sys:
             for frame in ls:
@@ -57,46 +78,58 @@ def run_ase_dptest(
                     energy_lab.append(frame.data["energies"])
                     energy_err.append(energy_predict - frame.data["energies"])
                     force_err.append(frame.data["forces"].squeeze(0) - np.array(ff))
-                    energy_err_per_atom.append(energy_err[-1]/force_err[-1].shape[0])
+                    energy_err_per_atom.append(energy_err[-1] / force_err[-1].shape[0])
                     try:
                         stress = atoms.get_stress()
-                        stress_tensor = - np.array(
-                        [[stress[0],stress[5],stress[4]],
-                        [stress[5],stress[1],stress[3]],
-                        [stress[4],stress[3], stress[2]]]
-                        )* atoms.get_volume()
-                        virial_err.append(frame.data['virials'] - stress_tensor)
-                        virial_err_per_atom.append(virial_err[-1]/force_err[-1].shape[0])
+                        stress_tensor = (
+                            -np.array(
+                                [
+                                    [stress[0], stress[5], stress[4]],
+                                    [stress[5], stress[1], stress[3]],
+                                    [stress[4], stress[3], stress[2]],
+                                ]
+                            )
+                            * atoms.get_volume()
+                        )
+                        virial_err.append(frame.data["virials"] - stress_tensor)
+                        virial_err_per_atom.append(
+                            virial_err[-1] / force_err[-1].shape[0]
+                        )
                     except:
                         pass
                 else:
                     pass
-        
-    
+
     atom_num = np.array(atom_num)
     energy_err = np.array(energy_err)
     energy_pre = np.array(energy_pre)
     energy_lab = np.array(energy_lab)
     shift_bias, _, _, _ = np.linalg.lstsq(atom_num, energy_err, rcond=1e-10)
-    unbiased_energy = energy_pre - (atom_num @ shift_bias.reshape(max_ele_num, -1)).reshape(-1) - energy_lab.squeeze()
+    unbiased_energy = (
+        energy_pre
+        - (atom_num @ shift_bias.reshape(max_ele_num, -1)).reshape(-1)
+        - energy_lab.squeeze()
+    )
     unbiased_energy_err_per_a = unbiased_energy / atom_num.sum(-1)
 
-
     res = {
-    "Energy MAE": [np.mean(np.abs(np.stack(unbiased_energy)))],
-    "Energy RMSE": [np.sqrt(np.mean(np.square(unbiased_energy)))],
-    "Energy MAE/Natoms": [np.mean(np.abs(np.stack(unbiased_energy_err_per_a)))],
-    "Energy RMSE/Natoms":  [np.sqrt(np.mean(np.square(unbiased_energy_err_per_a)))],
-    "Force MAE": [np.mean(np.abs(np.concatenate(force_err)))],
-    "Force RMSE": [np.sqrt(np.mean(np.square(np.concatenate(force_err))))],
+        "Energy MAE": [np.mean(np.abs(np.stack(unbiased_energy)))],
+        "Energy RMSE": [np.sqrt(np.mean(np.square(unbiased_energy)))],
+        "Energy MAE/Natoms": [np.mean(np.abs(np.stack(unbiased_energy_err_per_a)))],
+        "Energy RMSE/Natoms": [np.sqrt(np.mean(np.square(unbiased_energy_err_per_a)))],
+        "Force MAE": [np.mean(np.abs(np.concatenate(force_err)))],
+        "Force RMSE": [np.sqrt(np.mean(np.square(np.concatenate(force_err))))],
     }
     if virial_err_per_atom != []:
         res.update(
             {
-        "Virial MAE": [np.mean(np.abs(np.stack(virial_err)))],
-        "Virial RMSE": [np.sqrt(np.mean(np.square(np.stack(virial_err))))],
-        "Virial MAE/Natoms": [np.mean(np.abs(np.stack(virial_err_per_atom)))],
-        "Virial RMSE/Natoms": [np.sqrt(np.mean(np.square(np.stack(virial_err_per_atom))))]}
+                "Virial MAE": [np.mean(np.abs(np.stack(virial_err)))],
+                "Virial RMSE": [np.sqrt(np.mean(np.square(np.stack(virial_err))))],
+                "Virial MAE/Natoms": [np.mean(np.abs(np.stack(virial_err_per_atom)))],
+                "Virial RMSE/Natoms": [
+                    np.sqrt(np.mean(np.square(np.stack(virial_err_per_atom))))
+                ],
+            }
         )
     return res
 
@@ -107,9 +140,11 @@ def ase_test(model_name, testpath_mapping):
     """
     if model_name == "DP":
         from deepmd.calculator import DP
+
         CALC = DP("/path/to/your/model.pth")
     elif model_name == "MACE":
         from mace.calculators import mace_mp
+
         CALC = mace_mp(model="medium", device="cuda", default_dtype="float64")
     else:
         raise ValueError(f"Model {model_name} not supported.")
@@ -120,7 +155,7 @@ def ase_test(model_name, testpath_mapping):
         with open(f"{ood_name}_dptest.json", "w") as f:
             json.dump(head_dptest_res, f, indent=4)
 
-        
+
 if __name__ == "__main__":
     mapping = {
         "head_A": ["/path/to/your/test_data"],
--- lambench/models/ase_models.py
+++ lambench/models/ase_models.py
@@ -5,51 +5,63 @@ from pymatgen.io.ase import AseAtomsAdaptor
 from ase.calculators.calculator import Calculator
 import numpy as np
 import dpdata
+from dpdata.data_type import (
+    Axis,
+    DataType,
+)
 import glob
 from typing import Dict
+
 
 class ASEModel(BaseLargeAtomModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.model_type != "ASE":
-            raise ValueError(f"Model type {self.model_type} is not supported by ASEModel")
+            raise ValueError(
+                f"Model type {self.model_type} is not supported by ASEModel"
+            )
 
-    def evaluate(self, task_name:str, test_file_path: str, target_name: str):
+    def evaluate(self, task_name: str, test_file_path: str, target_name: str):
         if target_name != "standard":
             logging.error(f"ASEModel does not support target_name {target_name}")
             return {}
         else:
             if self.model_id.lower().startswith("mace"):
                 from mace.calculators import mace_mp
+
                 CALC = mace_mp(model="medium", device="cuda", default_dtype="float64")
             elif self.model_id.lower().startswith("orb"):
                 from orb_models.forcefield import pretrained
                 from orb_models.forcefield.calculator import ORBCalculator
-                orbff = pretrained.orb_v2(device="cuda") # orb-v2-20241011.ckpt
+
+                orbff = pretrained.orb_v2(device="cuda")  # orb-v2-20241011.ckpt
                 CALC = ORBCalculator(orbff, device="cuda")
             elif self.model_id.lower().startswith("7net"):
                 from sevenn.sevennet_calculator import SevenNetCalculator
-                CALC = SevenNetCalculator("7net-0_11July2024", device='cuda')
+
+                CALC = SevenNetCalculator("7net-0_11July2024", device="cuda")
             elif self.model_id.lower().startswith("eqv2"):
                 from fairchem.core import OCPCalculator
+
                 CALC = OCPCalculator(
-                    checkpoint_path="eqV2_153M_omat_mp_salex.pt",
-                    cpu=False
+                    checkpoint_path="eqV2_153M_omat_mp_salex.pt", cpu=False
                 )
             elif self.model_id.lower().startswith("mattersim"):
                 from mattersim.forcefield import MatterSimCalculator
-                CALC = MatterSimCalculator(load_path="MatterSim-v1.0.0-5M.pth", device="cuda")
+
+                CALC = MatterSimCalculator(
+                    load_path="MatterSim-v1.0.0-5M.pth", device="cuda"
+                )
             elif self.model_id.lower().startswith("dp"):
                 logging.error(f"Please use DPModel for DP models.")
                 return {}
             else:
                 logging.error(f"Model {self.model_id} is not supported by ASEModel")
                 return {}
-            return self.run_ase_dptest(CALC, test_file_path)
+            return self.run_ase_dptest(CALC, task_name, test_file_path)
 
-    
     @staticmethod
-    def run_ase_dptest(calc: Calculator,test_file_path: str) -> Dict:
+    def run_ase_dptest(calc: Calculator, task_name: str, test_file_path: str) -> Dict:
         adptor = AseAtomsAdaptor()
 
         energy_err = []
@@ -66,18 +78,18 @@ class ASEModel(BaseLargeAtomModel):
         for path in testpath:
             systems.extend(glob.glob(f"{path}/*"))
         # check if the system is mixed type
-        if len(glob.glob(systems[0] + '/**/real_atom_types.npy', recursive=True)) == 0:
+        if len(glob.glob(systems[0] + "/**/real_atom_types.npy", recursive=True)) == 0:
             mix_type = False
         else:
             mix_type = True
 
-
         for filepth in systems:
+            deregister_data_type(task_name)
             if mix_type:
                 sys = dpdata.MultiSystems()
-                sys.load_systems_from_file(filepth, fmt='deepmd/npy/mixed')
+                sys.load_systems_from_file(filepth, fmt="deepmd/npy/mixed")
             else:
-                sys = dpdata.LabeledSystem(filepth, fmt='deepmd/npy')
+                sys = dpdata.LabeledSystem(filepth, fmt="deepmd/npy")
 
             for ls in sys:
                 for frame in ls:
@@ -88,52 +100,89 @@ class ASEModel(BaseLargeAtomModel):
                     energy_predict = np.array(atoms.get_potential_energy())
                     if not np.isnan(energy_predict):
                         atomic_numbers = atoms.get_atomic_numbers()
-                        atom_num.append(np.bincount(atomic_numbers, minlength=max_ele_num))
+                        atom_num.append(
+                            np.bincount(atomic_numbers, minlength=max_ele_num)
+                        )
 
                         energy_pre.append(energy_predict)
                         energy_lab.append(frame.data["energies"])
                         energy_err.append(energy_predict - frame.data["energies"])
-                        force_err.append(frame.data["forces"].squeeze(0) - np.array(force_pred))
-                        energy_err_per_atom.append(energy_err[-1]/force_err[-1].shape[0])
+                        force_err.append(
+                            frame.data["forces"].squeeze(0) - np.array(force_pred)
+                        )
+                        energy_err_per_atom.append(
+                            energy_err[-1] / force_err[-1].shape[0]
+                        )
                         try:
                             stress = atoms.get_stress()
-                            stress_tensor = - np.array(
-                            [[stress[0],stress[5],stress[4]],
-                            [stress[5],stress[1],stress[3]],
-                            [stress[4],stress[3], stress[2]]]
-                            )* atoms.get_volume()
-                            virial_err.append(frame.data['virials'] - stress_tensor)
-                            virial_err_per_atom.append(virial_err[-1]/force_err[-1].shape[0])
+                            stress_tensor = (
+                                -np.array(
+                                    [
+                                        [stress[0], stress[5], stress[4]],
+                                        [stress[5], stress[1], stress[3]],
+                                        [stress[4], stress[3], stress[2]],
+                                    ]
+                                )
+                                * atoms.get_volume()
+                            )
+                            virial_err.append(frame.data["virials"] - stress_tensor)
+                            virial_err_per_atom.append(
+                                virial_err[-1] / force_err[-1].shape[0]
+                            )
                         except:
                             pass
                     else:
                         pass
-
 
         atom_num = np.array(atom_num)
         energy_err = np.array(energy_err)
         energy_pre = np.array(energy_pre)
         energy_lab = np.array(energy_lab)
         shift_bias, _, _, _ = np.linalg.lstsq(atom_num, energy_err, rcond=1e-10)
-        unbiased_energy = energy_pre - (atom_num @ shift_bias.reshape(max_ele_num, -1)).reshape(-1) - energy_lab.squeeze()
+        unbiased_energy = (
+            energy_pre
+            - (atom_num @ shift_bias.reshape(max_ele_num, -1)).reshape(-1)
+            - energy_lab.squeeze()
+        )
         unbiased_energy_err_per_a = unbiased_energy / atom_num.sum(-1)
 
-
         res = {
-        "Energy MAE": [np.mean(np.abs(np.stack(unbiased_energy)))],
-        "Energy RMSE": [np.sqrt(np.mean(np.square(unbiased_energy)))],
-        "Energy MAE/Natoms": [np.mean(np.abs(np.stack(unbiased_energy_err_per_a)))],
-        "Energy RMSE/Natoms":  [np.sqrt(np.mean(np.square(unbiased_energy_err_per_a)))],
-        "Force MAE": [np.mean(np.abs(np.concatenate(force_err)))],
-        "Force RMSE": [np.sqrt(np.mean(np.square(np.concatenate(force_err))))],
+            "Energy MAE": [np.mean(np.abs(np.stack(unbiased_energy)))],
+            "Energy RMSE": [np.sqrt(np.mean(np.square(unbiased_energy)))],
+            "Energy MAE/Natoms": [np.mean(np.abs(np.stack(unbiased_energy_err_per_a)))],
+            "Energy RMSE/Natoms": [
+                np.sqrt(np.mean(np.square(unbiased_energy_err_per_a)))
+            ],
+            "Force MAE": [np.mean(np.abs(np.concatenate(force_err)))],
+            "Force RMSE": [np.sqrt(np.mean(np.square(np.concatenate(force_err))))],
         }
         if virial_err_per_atom != []:
             res.update(
                 {
-            "Virial MAE": [np.mean(np.abs(np.stack(virial_err)))],
-            "Virial RMSE": [np.sqrt(np.mean(np.square(np.stack(virial_err))))],
-            "Virial MAE/Natoms": [np.mean(np.abs(np.stack(virial_err_per_atom)))],
-            "Virial RMSE/Natoms": [np.sqrt(np.mean(np.square(np.stack(virial_err_per_atom))))]}
+                    "Virial MAE": [np.mean(np.abs(np.stack(virial_err)))],
+                    "Virial RMSE": [np.sqrt(np.mean(np.square(np.stack(virial_err))))],
+                    "Virial MAE/Natoms": [
+                        np.mean(np.abs(np.stack(virial_err_per_atom)))
+                    ],
+                    "Virial RMSE/Natoms": [
+                        np.sqrt(np.mean(np.square(np.stack(virial_err_per_atom))))
+                    ],
+                }
             )
         return res
 
+    @staticmethod
+    def deregister_data_type(task_name: str):
+        """
+        Deregister data type to avoid conflict.
+        This should only be used in the WBM dataset which does not have force labels.
+        """
+        if task_name == "WBM_downsampled":
+            datatype = DataType(
+                "forces",
+                np.ndarray,
+                shape=(Axis.NFRAMES, Axis.NATOMS, 1),
+                required=False,
+            )
+            dpdata.System.register_data_type(datatype)
+            dpdata.LabeledSystem.register_data_type(datatype)
