cd9269766a8a9e43dbf212d7df4bcbb5404e1583
caic99 <amoycaic@gmail.com>
2025-01-10 11:08:25 +0800

    support nan in dp test result

--- lambench/tasks/utils/__init__.py
+++ lambench/tasks/utils/__init__.py
@@ -1,13 +1,20 @@
-import os
+import logging
+from math import isnan
 from pathlib import Path
+from typing import Optional
+
 
 def prepare_dptest_input_file():
     raise NotImplementedError
 
+
 def prepare_finetune_input_file():
     raise NotImplementedError
 
-def parse_dptest_log_file(filepath:Path, txt_type:str="standard") -> dict[str,float]:
+
+def parse_dptest_log_file(
+    filepath: Path, txt_type: str = "standard"
+) -> dict[str, Optional[float]]:
     """
     Parse dptest results to a dict
 
@@ -19,7 +26,7 @@ def parse_dptest_log_file(filepath:Path, txt_type:str="standard") -> dict[str,fl
             The type of dptest output file. Options are "standard" and "property".
 
     """
-    with open(filepath,"r") as f:
+    with open(filepath, "r") as f:
         content = f.readlines()
 
     if txt_type == "standard":
@@ -27,18 +34,28 @@ def parse_dptest_log_file(filepath:Path, txt_type:str="standard") -> dict[str,fl
         for line in content[-11:-1]:
             line = line.split("deepmd.entrypoints.test")[-1].strip()
             # Force  MAE/natoms -> force_mae_natoms in compliance with DirectPredictRecord
-            metrics[
+            key = (
                 line.split(":")[0]
                 .strip()
                 .replace("  ", " ")
                 .replace(" ", "_")
                 .replace("/", "_")
                 .lower()
-            ] = float(line.split(":")[-1].strip().split(" ")[0])
+            )
+            value = float(line.split(":")[-1].strip().split(" ")[0])
+            if isnan(value):
+                value = None  # MySQL does not support NaN
+            metrics[key] = value
     elif txt_type == "property":
         metrics = {}
-        metrics["PROPERTY MAE"] = float(content[-3].split(":")[-1].split("units")[0].strip())
-        metrics["PROPERTY RMSE"] = float(content[-2].split(":")[-1].split("units")[0].strip())
+        metrics["PROPERTY MAE"] = float(
+            content[-3].split(":")[-1].split("units")[0].strip()
+        )
+        metrics["PROPERTY RMSE"] = float(
+            content[-2].split(":")[-1].split("units")[0].strip()
+        )
     else:
         raise ValueError(f"Unknown dptest output type: {txt_type}")
+    if all(value is None for value in metrics.values()):
+        logging.warning("All metrics are NaN. Something went wrong.")
     return metrics
