a016aa6a42052c1a607152f19da62c402264510f
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2025-01-15 11:03:20 +0800

    fix: UT

--- lambench/models/basemodel.py
+++ lambench/models/basemodel.py
@@ -4,16 +4,17 @@ from pydantic import BaseModel
 from enum import Enum
 from abc import abstractmethod
 
+
 class ModelType(str, Enum):
     DP = "DP"
     ASE = "ASE"
+
 
 class BaseLargeAtomModel(BaseModel):
     model_name: str
     model_type: ModelType
     model_path: Optional[Path]
     virtualenv: str
-    model_metadata: dict[str, str]
 
     @abstractmethod
     def evaluate(self, task) -> dict[str, float]:
