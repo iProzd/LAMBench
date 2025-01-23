caf5c8e9f008f14f5572435e16d3cc07a6e44e5e
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2025-01-23 14:47:58 +0800

    fix: imports

--- lambench/tasks/__init__.py
+++ lambench/tasks/__init__.py
@@ -1,7 +1,9 @@
 from lambench.tasks.direct.direct_tasks import DirectPredictTask
 from lambench.tasks.finetune.property_finetune import PropertyFinetuneTask
+from lambench.tasks.calculator.calculator_tasks import CalculatorTask
 
 __all__ = [
     "DirectPredictTask",
     "PropertyFinetuneTask",
+    "CalculatorTask",
 ]
