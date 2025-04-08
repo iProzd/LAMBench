1425ac4a844ec3a1186f4a47ae5d10b1d605f5b6
caic99 <amoycaic@gmail.com>
2025-04-08 09:19:28 +0000

    feat: add torsionnet as calculator task

---
+++ lambench/tasks/calculator/torsionnet/torsionnet.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from ase import Atoms
+import dpdata
+import sklearn.metrics as metrics
+import pandas as pd
+from lambench.models.ase_models import ASEModel
+from lambench.models.dp_models import DPModel
+from tqdm import tqdm
+
+
+def run_torsionnet(
+    model: ASEModel,
+    test_data: Path,
+) -> dict[str, float]:
+    metric = {}
+    result = {}  # predicted energy
+    label = {}  # label energy
+
+    # 500 fragments from Torsionnet500 dataset
+    for fragment in tqdm(test_data.iterdir()):
+        if not fragment.is_dir():
+            continue
+        result[fragment.name] = []
+        label[fragment.name] = []
+        # 24 conformations for each fragment
+        for frame in dpdata.LabeledSystem(file_name=fragment, fmt="deepmd/raw"):
+            assert len(frame) == 1
+            atoms: Atoms = frame.to_ase_structure()[0]  # type: ignore
+            atoms.calc = model.calc
+            pred_energy = atoms.get_potential_energy()
+            result[fragment.name].append(pred_energy)
+            label_energy = frame.data["energies"][0]
+            label[fragment.name].append(label_energy)
+    result_df = pd.DataFrame.from_dict(result, orient="index")
+    label_df = pd.DataFrame.from_dict(label, orient="index")
+
+    # barrier height MAE (barrier height is the max energy - min energy of the fragment)
+    result_barrier = result_df.max(axis=1) - result_df.min(axis=1)
+    label_barrier = label_df.max(axis=1) - label_df.min(axis=1)
+    metric["barrier_mae"] = metrics.mean_absolute_error(
+        label_barrier,
+        result_barrier,
+    )
+
+    # percentage of molecules with error of a barrier highr more than 1 kcal/mol
+    barrier_diff = (result_barrier - label_barrier).abs()
+    print(barrier_diff.mean(axis=None))
+    metric["percentage"] = sum(barrier_diff > (1 / 23.0609)) / len(barrier_diff)
+
+    # normalize the energies
+    result_df = result_df.sub(result_df.min(axis=1), axis=0)
+    ## label is already normalized
+    # label_df = label_df.sub(label_df.min(axis=1), axis=0)
+    assert label_df.min(axis=1).max(axis=0) == 0
+    metric["energy_mae"] = metrics.mean_absolute_error(label_df, result_df)
+    metric["energy_rmse"] = metrics.root_mean_squared_error(label_df, result_df)
+    return metric
