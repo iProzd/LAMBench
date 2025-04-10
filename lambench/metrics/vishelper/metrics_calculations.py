fix: aligh metrics calculation

lambench/metrics/vishelper/metrics_calculations.py
@@ -1,5 +1,4 @@
 import logging
-from typing import Optional
 import numpy as np
 import pandas as pd
 from collections import defaultdict
@@ -21,7 +20,6 @@ class MetricsCalculator:
         self,
         metric_dict: dict[str, float],
         method: str = "minmax",
-        dummy: Optional[float] = None,
     ) -> dict[str, float]:
         """Convert metric values (where lower is better) to normalized scores in range [0, 1] (where higher is better)."""
         if not metric_dict:
@@ -29,10 +27,7 @@ class MetricsCalculator:
         scores = {}
         if method == "minmax":
             min_value = min(metric_dict.values())
-            if dummy is not None:
-                max_value = dummy
-            else:
-                max_value = max(metric_dict.values())
+            max_value = max(metric_dict.values())
 
             # If all values are the same, return 1.0 for all
             if max_value == min_value:
@@ -81,16 +76,15 @@ class MetricsCalculator:
             for domain, metrics in reorg_m_bar_domain.items()
         }
 
-        # Calculate average score across all domains for each model
-        mean_s_domain = defaultdict(list)
+        # Calculate final generalizability score as mean across domains
+        s_domain_values = defaultdict(list)
         for domain_scores in s_domain.values():
             for model, score in domain_scores.items():
-                mean_s_domain[model].append(score)
+                s_domain_values[model].append(score)
 
-        # Calculate final generalizability score as mean across domains
         generalizability_scores = {
             model: np.mean(scores)
-            for model, scores in mean_s_domain.items()
+            for model, scores in s_domain_values.items()
             if scores  # Skip models with no scores
         }
 
@@ -98,15 +92,31 @@ class MetricsCalculator:
 
     def calculate_generalizability_downstream_score(self) -> dict[str, float]:
         raw_results = self.fetcher.fetch_downstream_results()
-        penalty_dict = {}
-        for task_name, task_config in DOWNSTREAM_TASK_METRICS.items():
-            if task_config.get("penalty", None) is not None:
-                penalty_dict[f'{task_name}::{task_config["penalty"]}'] = [
-                    f"{task_name}::{metrics_name}"
-                    for metrics_name in task_config["metrics"]
-                ]
 
-        # apply minmax normalization for all metrics, use dummy when available
+        # Extract necessary columns and prepare penalty dict
+        necessary_columns = []
+        penalty_dict = {}
+        domain_columns = defaultdict(list)  # use in domain level aggregation
+        for task_name, task_config in DOWNSTREAM_TASK_METRICS.items():
+            # Add all metric columns
+            metrics_columns = [
+                f"{task_name}::{metrics_name}"
+                for metrics_name in task_config["metrics"]
+            ]
+            domain_columns[task_config["domain"]].extend(metrics_columns)
+            necessary_columns.extend(metrics_columns)
+
+            # Add penalty column and mapping if available
+            if "penalty" in task_config:
+                penalty_column = f'{task_name}::{task_config["penalty"]}'
+                necessary_columns.append(penalty_column)
+                penalty_dict[penalty_column] = metrics_columns
+
+        # Filter dataframe to include only the necessary columns
+        raw_results = raw_results[necessary_columns]
+
+        # Normalize all metrics by dummy baseline dimensionless metrics
+        # This gives values equivalent to $\bar{M}_i$, where $i$ is the task name, no log average needed
         for column in raw_results.columns:
             if column not in penalty_dict:
                 check_dummy = column.split(
@@ -115,7 +125,6 @@ class MetricsCalculator:
                 assert (
                     len(check_dummy) == 2
                 ), f"Column name {column} is not in the expected format"
-                # extract dummy max value when available
                 if (
                     DOWNSTREAM_TASK_METRICS[check_dummy[0]].get("dummy", None)
                     is not None
@@ -125,42 +134,44 @@ class MetricsCalculator:
                     ]
                 else:
                     dummy = None
-                raw_results[column] = self.convert_metric_to_score(
-                    raw_results[column].to_dict(), method="minmax", dummy=dummy
-                )
 
-        # apply penalty for each task
-        for task_name, penalty in penalty_dict.items():
-            for penalty_metric in penalty:
-                if penalty_metric not in raw_results.columns:
-                    logging.error(f"Metric {penalty_metric} not found in raw results")
+                if dummy is None:
+                    logging.warning(
+                        f"Dummy value not found for {column}, skipping normalization"
+                    )
+                else:
+                    # normalize the metric by dummy value
+                    raw_results[column] = raw_results[column] / dummy
+
+        # Apply penalty for specified metrics directly to $\bar{M}_i$ before domain level aggregation.
+        # $\bar{M}_i$ is an error metric, the lower the better, so we want to penalize it by dividing
+        # by the penalty column (success rate in range [0,1])
+        for penalty_column, metrics_to_penalize in penalty_dict.items():
+            for metric in metrics_to_penalize:
+                if metric not in raw_results.columns:
+                    logging.error(f"Metric {metric} not found in raw results")
                     continue
-                # apply penalty
-                # Apply penalty based on sign of the metric
-                positive_mask = raw_results[penalty_metric] >= 0
-                negative_mask = raw_results[penalty_metric] < 0
-
-                # Create a copy to avoid SettingWithCopyWarning
-                new_values = raw_results[penalty_metric].copy()
-
-                # For positive values, multiply
-                new_values[positive_mask] = (
-                    raw_results[penalty_metric][positive_mask]
-                    * raw_results[task_name][positive_mask]
-                )
-
-                # For negative values, divide
-                new_values[negative_mask] = (
-                    raw_results[penalty_metric][negative_mask]
-                    / raw_results[task_name][negative_mask]
-                )
-
-                raw_results[penalty_metric] = new_values
-
-        # aggregate all metrics for each model
-        downstream_score = raw_results.drop(columns=penalty_dict.keys()).mean(axis=1)
+                raw_results[metric] = raw_results[metric] / raw_results[penalty_column]
         print(raw_results)
-        return downstream_score.to_dict()
+
+        # Aggregate all metrics for each domain to get domain level error metrics equivalent to $\bar{M}_{\text{domain}}$
+        domain_level_metrics = {}
+        for domain, columns in domain_columns.items():
+            domain_df = raw_results[columns]
+            domain_level_metrics[domain] = domain_df.mean(axis=1)
+
+        domain_results = pd.DataFrame(domain_level_metrics)
+
+        # Now convert each domain's metrics to scores (0-1 where higher is better), equivalent to $S_{\text{domain}}$
+        domain_scores = {}
+        for domain in domain_results.columns:
+            domain_scores[domain] = self.convert_metric_to_score(
+                domain_results[domain].to_dict(), method="-log"
+            )
+
+        domain_results = pd.DataFrame(domain_scores)
+        # Now aggregate all domains to get the final generalizability score for each model
+        return domain_results.mean(axis=1).to_dict()
 
     def calculate_stability_results(self) -> dict[str, float]:
         """This calculates the stability score for a given LAM."""